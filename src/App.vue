45a7d5011fa791be82d10162b8f4434c9a7c4996
王啸 <wangxiao@192.168.1.5>
2021-02-21 20:18:30 +0800

    搜索界面编写

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,109 @@
 <template>
-  <div id="app">
-    <el-button type="primary" icon="el-icon-search">搜索</el-button>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div style="margin-top: 15px;">
 
-     <video-player  class="video-player-box"
-                 ref="videoPlayer"
-                 :options="playerOptions"
-                 :playsinline="true"
-                 customEventName="customstatechangedeventname"
+    <img src="../src/assets/bilibili-backTop.jpg" />
+    <el-menu :default-active="activeIndex"
+             class="el-menu-demo"
+             mode="horizontal"
+             @select="handleSelect">
+      <el-menu-item index="1">视频搜索</el-menu-item>
+      <el-menu-item index="2">上传视频</el-menu-item>
 
-                 @play="onPlayerPlay($event)"
-                 @pause="onPlayerPause($event)"
-                 @ended="onPlayerEnded($event)"
-                 @waiting="onPlayerWaiting($event)"
-                 @playing="onPlayerPlaying($event)"
-                 @loadeddata="onPlayerLoadeddata($event)"
-                 @timeupdate="onPlayerTimeupdate($event)"
-                 @canplay="onPlayerCanplay($event)"
-                 @canplaythrough="onPlayerCanplaythrough($event)"
-
-                 @statechanged="playerStateChanged($event)"
-                 @ready="playerReadied">
-  </video-player>
+    </el-menu>
+    <el-col :span="12"
+            :offset="6">
+      <h1>视频搜索界面</h1>
+    </el-col>
+    <el-row :gutter="20">
+      <el-col :span="12"
+              :offset="6">
+        <el-input placeholder="请输入内容"
+                  v-model="input3"
+                  class="input-with-select">
+          <el-select v-model="select"
+                     slot="prepend"
+                     placeholder="请选择搜索类型">
+            <el-option label="视频名"
+                       value="1"></el-option>
+            <el-option label="视频id"
+                       value="2"></el-option>
+          </el-select>
+          <el-button slot="append"
+                     icon="el-icon-search"></el-button>
+        </el-input>
+      </el-col>
+    </el-row>
+    <el-row :gutter="20"
+            :offset="3"
+            :justify="bottom">
+      <el-col :span="12"
+              :offset="8"
+              :justify="bottom">
+        <div class="grid-content bg-purple">
+          <el-pagination background
+                         layout="prev, pager, next"
+                         :total="1000"></el-pagination>
+        </div>
+      </el-col>
+    </el-row>
 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   },
   //添加视频播放器
-   data() {
-      return {
-        playerOptions: {
-          // videojs options
-          muted: true,
-          language: 'zn-ch',
-          playbackRates: [0.7, 1.0, 1.5, 2.0],
-          sources: [{
-            type: "video/mp4",
-            src: "https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm"
-          }],
-          poster: "/static/images/author.jpg",
-        }
-      }
-    },
-    mounted() {
-      console.log('this is current player instance object', this.player)
-    },
-    computed: {
-      player() {
-        return this.$refs.videoPlayer.player
-      }
-    },
-    methods: {
-      // listen event
-      onPlayerPlay(player) {
-        console.log('player play!', player)
-      },
-      onPlayerPause(player) {
-        console.log('player pause!', player)
-      },
-      // ...player event
-
-      // or listen state event
-      playerStateChanged(playerCurrentState) {
-        console.log('player current update state', playerCurrentState)
-      },
-
-      // player is ready
-      playerReadied(player) {
-        console.log('the player is readied', player)
-        // you can use it to do something...
-        // player.[methods]
+  data () {
+    return {
+      playerOptions: {
+        // videojs options
+        muted: true,
+        language: 'zn-ch',
+        playbackRates: [0.7, 1.0, 1.5, 2.0],
+        sources: [{
+          type: "video/mp4",
+          src: "https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm"
+        }],
+        poster: "/static/images/author.jpg",
       }
     }
+  },
+  mounted () {
+    console.log('this is current player instance object', this.player)
+  },
+  computed: {
+    player () {
+      return this.$refs.videoPlayer.player
+    }
+  },
+  methods: {
+    // listen event
+    onPlayerPlay (player) {
+      console.log('player play!', player)
+    },
+    onPlayerPause (player) {
+      console.log('player pause!', player)
+    },
+    // ...player event
+
+    // or listen state event
+    playerStateChanged (playerCurrentState) {
+      console.log('player current update state', playerCurrentState)
+    },
+
+    // player is ready
+    playerReadied (player) {
+      console.log('the player is readied', player)
+      // you can use it to do something...
+      // player.[methods]
+    }
+  }
 }
 </script>
 
--- src/components/video_play.vue
+++ src/components/video_play.vue
@@ -1,27 +1,26 @@
 <template>
   <div id="app">
-    <el-button type="primary" icon="el-icon-search">搜索</el-button>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-button type="primary"
+               icon="el-icon-search">搜索</el-button>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
 
-     <video-player  class="video-player-box"
-                 ref="videoPlayer"
-                 :options="playerOptions"
-                 :playsinline="true"
-                 customEventName="customstatechangedeventname"
-
-                 @play="onPlayerPlay($event)"
-                 @pause="onPlayerPause($event)"
-                 @ended="onPlayerEnded($event)"
-                 @waiting="onPlayerWaiting($event)"
-                 @playing="onPlayerPlaying($event)"
-                 @loadeddata="onPlayerLoadeddata($event)"
-                 @timeupdate="onPlayerTimeupdate($event)"
-                 @canplay="onPlayerCanplay($event)"
-                 @canplaythrough="onPlayerCanplaythrough($event)"
-
-                 @statechanged="playerStateChanged($event)"
-                 @ready="playerReadied">
-  </video-player>
+    <video-player class="video-player-box"
+                  ref="videoPlayer"
+                  :options="playerOptions"
+                  :playsinline="true"
+                  customEventName="customstatechangedeventname"
+                  @play="onPlayerPlay($event)"
+                  @pause="onPlayerPause($event)"
+                  @ended="onPlayerEnded($event)"
+                  @waiting="onPlayerWaiting($event)"
+                  @playing="onPlayerPlaying($event)"
+                  @loadeddata="onPlayerLoadeddata($event)"
+                  @timeupdate="onPlayerTimeupdate($event)"
+                  @canplay="onPlayerCanplay($event)"
+                  @canplaythrough="onPlayerCanplaythrough($event)"
+                  @statechanged="playerStateChanged($event)"
+                  @ready="playerReadied">
+    </video-player>
 
   </div>
 </template>
@@ -35,51 +34,51 @@ export default {
     HelloWorld
   },
   //添加视频播放器
-   data() {
-      return {
-        playerOptions: {
-          // videojs options
-          muted: true,
-          language: 'zn-ch',
-          playbackRates: [0.7, 1.0, 1.5, 2.0],
-          sources: [{
-            type: "video/mp4",
-            src: "https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm"
-          }],
-          poster: "/static/images/author.jpg",
-        }
-      }
-    },
-    mounted() {
-      console.log('this is current player instance object', this.player)
-    },
-    computed: {
-      player() {
-        return this.$refs.videoPlayer.player
-      }
-    },
-    methods: {
-      // listen event
-      onPlayerPlay(player) {
-        console.log('player play!', player)
-      },
-      onPlayerPause(player) {
-        console.log('player pause!', player)
-      },
-      // ...player event
-
-      // or listen state event
-      playerStateChanged(playerCurrentState) {
-        console.log('player current update state', playerCurrentState)
-      },
-
-      // player is ready
-      playerReadied(player) {
-        console.log('the player is readied', player)
-        // you can use it to do something...
-        // player.[methods]
+  data () {
+    return {
+      playerOptions: {
+        // videojs options
+        muted: true,
+        language: 'zn-ch',
+        playbackRates: [0.7, 1.0, 1.5, 2.0],
+        sources: [{
+          type: "video/mp4",
+          src: "https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm"
+        }],
+        poster: "/static/images/author.jpg",
       }
     }
+  },
+  mounted () {
+    console.log('this is current player instance object', this.player)
+  },
+  computed: {
+    player () {
+      return this.$refs.videoPlayer.player
+    }
+  },
+  methods: {
+    // listen event
+    onPlayerPlay (player) {
+      console.log('player play!', player)
+    },
+    onPlayerPause (player) {
+      console.log('player pause!', player)
+    },
+    // ...player event
+
+    // or listen state event
+    playerStateChanged (playerCurrentState) {
+      console.log('player current update state', playerCurrentState)
+    },
+
+    // player is ready
+    playerReadied (player) {
+      console.log('the player is readied', player)
+      // you can use it to do something...
+      // player.[methods]
+    }
+  }
 }
 </script>
 
